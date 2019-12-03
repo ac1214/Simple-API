829632e49ddbfb73589ab232088e5d04e1b53833
A <a@pop-os.localdomain>
2019-12-03 13:01:58 -0700

    endpoints working

--- app.py
+++ app.py
@@ -8,10 +8,34 @@ import os
 #init flask app
 app = Flask(__name__)
 
+items = set()
 
 @app.route('/')
 def home_endpoint():
     return 'Hello World!'
+
+@app.route('/create/', methods=['PUT'])
+def create():
+    item = request.args.get('item')
+    print("testing item: " + item, file=sys.stderr)
+
+    if(item in items):
+        return "item already exists"
+    else:
+        items.add(item)
+        return "pass"
+
+@app.route('/delete/', methods=['GET'])
+def delete():
+    item = request.args.get('item')
+    print("testing item: " + item, file=sys.stderr)
+
+    if(item in items):
+        items.remove(item)
+        return "pass"
+    else:
+        return "item does not exist"
+
 
 
 if (__name__ == '__main__'):
